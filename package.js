b4d7d350734fd3903f35c90aac422a11d1c9cbc5
Fernando Mumbach <fermuch@cryptolab.net>
2015-03-26 16:01:22 -0300

    version bump for windows

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   summary: 'port of node-msgpack',
-  version: "0.1.0",
+  version: "0.1.1",
   git: "https://github.com/fermuch/meteor-msgpack"
 });
 
 Npm.depends({
-  "msgpack": "0.2.4"
+  "msgpack": "0.2.6"
 });
 
 Package.onUse(function(api) {
